fd71c743b64bf3a871ccef215be876cadd610b64
OijerTeSemyon <69199365+OijerTeSemyon@users.noreply.github.com>
2022-04-11 15:19:24 +0800

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+echo ---------------wsayyds!---------------
+python wsa.py
+echo [TIP] if error: list index out of range,it means none pic in Originalpic
+pause
--- wsa.py
+++ wsa.py
@@ -1,33 +1,30 @@
 import cv2
 from PIL import ImageFont, ImageDraw, Image
 import numpy as np
+import pandas as pd
+import os
+import shutil
 
-bk_img = cv2.imread("1.jpg")
+name_list = os.listdir("./Originalpic/")
+houzhui = name_list[0][name_list[0].find('.'):]
+for i in range(len(name_list)):
+    name = name_list[i]
+    shutil.move("./Originalpic/"+name,"./"+"originalpic"+houzhui)
 
-filenamelist=["B软件201_200107121018_汤靖云.jpg","B软件201_200107121022_王麒语.jpg","B软件201_200107121020_王超强.jpg"]
-namelist=["汤靖云","王麒语","王超强"]
-filename=""
-name=""
-
-str=input()
-if (str == '18'):
-    filename = filenamelist[0]
-    name = namelist[0]
-elif (str=='22'):
-    filename = filenamelist[1]
-    name = namelist[1]
-
-elif (str=='20'):
-    filename = filenamelist[2]
-    name = namelist[2]
-
+file = pd.read_excel(io='./user.xls',sheet_name=0,usecols=[0,1])
+filenamelist=file["filenamelist"]
+namelist=file["namelist"]
 fontpath = "font/simsun.ttc"
-font = ImageFont.truetype(fontpath, 320)
-img_pil = Image.fromarray(bk_img)
-draw = ImageDraw.Draw(img_pil)
 
-draw.text((100, 350),  name, font = font, fill = (255, 0, 0))
-bk_img = np.array(img_pil)
 
-cv2.waitKey()
-cv2.imencode('.jpg', bk_img)[1].tofile(filename)
+for i in range(len(filenamelist)):
+    bk_img = cv2.imread("originalpic.jpg")
+    font = ImageFont.truetype(fontpath, 320)
+    img_pil = Image.fromarray(bk_img)
+    draw = ImageDraw.Draw(img_pil)
+    draw.text((100, 350),  namelist[i], font = font, fill = (255, 0, 0))
+    bk_img = np.array(img_pil)
+    cv2.waitKey()
+    cv2.imencode('.jpg', bk_img)[1].tofile(filenamelist[i])
+
+print("Success!")
